66590af0a872f6ae4ca54d180eb37065129dc800
Juliodlc122 <julio.ant122@outlook.com>
2025-06-04 21:42:37 -0400

    Uploading homework 2

---
+++ Homework 2/Even&Odd/Even&Odd/Program.cs
@@ -0,0 +1,19 @@
+﻿// Julio Ruiz Matricula:2024-2009
+Console.Write("Type the number: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("The number is even.");
+    }
+    else
+    {
+        Console.WriteLine("The number is odd.");
+    }
+}
+else
+{
+    Console.WriteLine("It isn't a number.");
+}
